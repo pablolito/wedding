7673505f266e16afdcb55bde042634af913aed19
mfalguier <maxime.falguier@ux-republic.com>
2016-02-22 01:50:38 +0100

    rsvp wip

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,22 @@
 /* main */
-var ui = {};
+var decal = 0;
+var registrationStepByStep = {
+	containerForm: ".form-track",
+	nbFieldset : 4,
+	fieldsetWidth : 648,
+	init : function(){
+		var containerForm = document.querySelectorAll(this.containerForm);
+		for (var i = 0; i < containerForm.length; i++) {
+			$("fieldset").css("width", this.fieldsetWidth);
+		  containerForm[i].style.width = this.nbFieldset*this.fieldsetWidth+"px";
+		}
+	},
+	nextStep : function(){
+		decal = decal + this.fieldsetWidth;
+		var myForm = document.querySelectorAll(this.containerForm);
+		myForm[0].style.margin = "0 0 0 -"+decal+"px";
+	}
+};
 
 $(function(){
 	var $header = $("header"),
@@ -25,6 +42,22 @@ $(function(){
 			}, "slow");
 		}
 	});
+
+
+	$('input:radio[name="goingBottom"]').change(function(){
+		var formIndex = $(this).val();
+		$(".hidden").hide();
+        $(".hidden").eq(formIndex).show("normal", function(){
+        	if(formIndex==1){
+        		var fieldSetWidth = $(".form-track").outerWidth();
+	        	registrationStepByStep.fieldsetWidth = fieldSetWidth;
+	        	registrationStepByStep.init();
+        	}
+        });
+    });
+    $(".next-step").on("click", function(){
+    	registrationStepByStep.nextStep();
+    })
 	
 	/* event scroll */
 	$(window).scroll(function(e) {
@@ -37,4 +70,27 @@ $(function(){
 			$body.css("padding-top", 0);
 		}
 	});
+
+	
+	/* Material design field */
+	$('.field-input').focus(function(){
+		$(this).parent().addClass('is-focused has-label');
+	})
+
+	$('.field-input').blur(function(){
+		$parent = $(this).parent();
+
+		if($(this).val() == ''){
+			$parent.removeClass('has-label');
+		}
+
+		$parent.removeClass('is-focused');
+	});
 });
+
+	
+
+
+
+
+
